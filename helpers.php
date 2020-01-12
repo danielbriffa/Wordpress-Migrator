27423cd45380097c7d63fab8ccf67310b9910e4f
Daniel Briffa <danielbriffa@live.com>
2020-01-12 17:00:04 +0100

    Project init - POC

---
+++ helpers.php
@@ -0,0 +1,50 @@
+<?php
+
+//https://gist.github.com/jermity/af85cdcaabdb36f96173
+function getUrlsFromString($string) 
+{
+    $regex = '/https?\:\/\/[^\" \n]+/i';
+    preg_match_all('#\bhttps?://[^,\s()<>]+(?:\([\w\d]+\)|([^,[:punct:]\s]|/))#', $string, $match);
+    
+    return $match[0];
+}
+
+function urlIsImage($url)
+{
+    if (in_array(substr($url, strlen($url)-3), ['jpg', 'png']))
+    {
+        return true;
+    }
+    
+    return false;
+}
+
+function makeCurlCall($username, $password, $url, $data)
+{
+    try
+    {
+        $ch = curl_init();
+
+        curl_setopt( $ch, CURLOPT_URL, $url );
+        curl_setopt( $ch, CURLOPT_POST, 1 );
+        curl_setopt( $ch, CURLOPT_POSTFIELDS, $data );
+        curl_setopt($ch, CURLOPT_RETURNTRANSFER, TRUE);
+        curl_setopt( $ch, CURLOPT_HTTPHEADER, [
+            'Content-Disposition: form-data; filename="example.jpg"',
+            'Authorization: Basic ' . base64_encode( $username . ':' . $password ),
+        ] );
+    
+        $result = curl_exec( $ch );
+
+        curl_close( $ch );
+
+        return json_decode($result);
+        
+    }
+    catch(Exception $e)
+    {
+        var_dump($e);
+    }
+}
+
+  
---
+++ index.php
@@ -0,0 +1,84 @@
+<?php
+
+ini_set('max_execution_time', 600); //10 mins
+
+include_once('helpers.php');
+
+const ORIGIN_BLOG_URL = '###/wp-json/wp/v2/posts';
+const DESTINATION_WORDPRESS_REST_URL = 'http://localhost:8080/wp-json/';
+
+const OLD_DOMAIN = '###';
+const NEW_DOMAIN = 'localhost:8080';
+
+const WP_MEDIA_URL = DESTINATION_WORDPRESS_REST_URL.'wp/v2/media/';
+const WP_POST_URL = '';
+
+const USERNAME = '###';
+const PASSWORD = '###';
+
+$urls_to_replace = [];
+
+//Get Posts
+$json = stripcslashes(file_get_contents(ORIGIN_BLOG_URL));
+
+//Extract URLs
+$urls = getUrlsFromString($json);
+//Remove dupliactes
+$unique_urls = array_unique($urls);
+
+foreach($unique_urls as $url) 
+{
+
+  //Check if URL is of image
+  if(urlIsImage($url))
+  {
+    try
+    {
+      //get image and call destination api to store image
+      $image = file_get_contents($url);
+      
+      $response = makeCurlCall(USERNAME, PASSWORD, WP_MEDIA_URL, $image);
+
+      $uploaded_image_url = $response->guid->rendered; //TO DO -- GET THE URL OF THE NEWLY UPLOADED IMAGE
+
+      $urls_to_replace[$url] = $uploaded_image_url;
+
+      break;
+    }
+    catch(Exception $e){
+      var_dump($e);
+    }        
+  }
+  else 
+  {
+    //if url is not an image, in case it is an internal link, replace with new domain
+    $urls_to_replace[$url] = str_replace(OLD_DOMAIN, NEW_DOMAIN, $url);
+  }
+}
+
+//replace urls in JSON response
+foreach ($urls_to_replace as $old_url=>$new_url)
+{
+  $json = str_replace($old_url, $new_url, $json);
+}
+
+//insert posts
+var_dump($json);
+
+
+
+//plan
+
+//go through array and extract URLs
+
+//Loop through array 
+  //if attachment
+    //Retrieve attachements 
+    //store them locally
+    //update old domain to new
+  //if link
+    //update old domain to new
+
+//update json with new references
+
+//insert post
